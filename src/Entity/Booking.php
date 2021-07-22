5a3fa0a9e3c0191f5a64ccd5f81027b5dfda8783
crisalejos <cristina-alejos@outlook.com>
2021-07-22 11:39:57 +0200

    creation user class

---
+++ src/Entity/Booking.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookingRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BookingRepository::class)
+ */
+class Booking
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $startdate;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $enddate;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $contractdate;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="bookings")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Cars::class, inversedBy="bookings")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $car;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStartdate(): ?\DateTimeInterface
+    {
+        return $this->startdate;
+    }
+
+    public function setStartdate(\DateTimeInterface $startdate): self
+    {
+        $this->startdate = $startdate;
+
+        return $this;
+    }
+
+    public function getEnddate(): ?\DateTimeInterface
+    {
+        return $this->enddate;
+    }
+
+    public function setEnddate(\DateTimeInterface $enddate): self
+    {
+        $this->enddate = $enddate;
+
+        return $this;
+    }
+
+    public function getContractdate(): ?\DateTimeInterface
+    {
+        return $this->contractdate;
+    }
+
+    public function setContractdate(\DateTimeInterface $contractdate): self
+    {
+        $this->contractdate = $contractdate;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getCar(): ?Cars
+    {
+        return $this->car;
+    }
+
+    public function setCar(?Cars $car): self
+    {
+        $this->car = $car;
+
+        return $this;
+    }
+}
--- src/Entity/Cars.php
+++ src/Entity/Cars.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CarsRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -43,6 +45,16 @@ class Cars
      * @ORM\JoinColumn(nullable=false)
      */
     private $color;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Booking::class, mappedBy="car", orphanRemoval=true)
+     */
+    private $bookings;
+
+    public function __construct()
+    {
+        $this->bookings = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -105,6 +117,36 @@ class Cars
     public function setColor(?CarColor $color): self
     {
         $this->color = $color;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Booking[]
+     */
+    public function getBookings(): Collection
+    {
+        return $this->bookings;
+    }
+
+    public function addBooking(Booking $booking): self
+    {
+        if (!$this->bookings->contains($booking)) {
+            $this->bookings[] = $booking;
+            $booking->setCar($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBooking(Booking $booking): self
+    {
+        if ($this->bookings->removeElement($booking)) {
+            // set the owning side to null (unless already changed)
+            if ($booking->getCar() === $this) {
+                $booking->setCar(null);
+            }
+        }
 
         return $this;
     }
